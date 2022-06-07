b63b5665af8c4ac4f09067944b6645e1b33da650
contactharrisc2 <contactharrisc2@gmail.com>
2022-06-06 22:05:29 -0500

    updates App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css';
+import {useState, useEffect} from 'react';
 import Header from './components/Header';
 import Home from './components/pages/Home';
 import NewWine from './components/pages/NewWine';
 
 function App() {
+  
   return (
     <div className="App">
       <Header/>
