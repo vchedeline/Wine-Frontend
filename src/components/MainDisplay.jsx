0eb359505234ad78d3b9eb2a5f4aca9eee90d0df
Nathan Yi <nathanyi112@gmail.com>
2022-06-07 22:39:40 -0500

    adds MainDisplay

--- src/components/MainDisplay.jsx
+++ src/components/MainDisplay.jsx
@@ -1,9 +1,27 @@
 
 
-export default function MainDisplay() {
+export default function MainDisplay({ wineList }) {
+
+    const loaded = () => {
+        return wineList.map((wine, index) => {
+            return(
+                <div key={index}>
+                    name={wine.name}
+                    type={wine.type}
+                    year={wine.year}
+                    price={wine.price}
+                    details={wine.details}
+                    image={wine.image}
+                </div>
+            )
+        })
+    }
+
+    const loading = () => <h1>Loading...</h1>
+
     return(
         <section className="Main-Disp">
-            <h1>MainDisplay</h1>
+            {wineList ? loaded() : loading()}
         </section>
     )
 }
