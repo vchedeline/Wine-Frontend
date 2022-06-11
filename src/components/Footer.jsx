897009b0a838653d625518aff98e358548235315
contactharrisc2 <contactharrisc2@gmail.com>
2022-06-11 17:13:58 -0500

    updates footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,33 +3,12 @@
 export default function Footer() {
     return(
         <div className="Footer">
-            <h2>Vineyards Dev Team:</h2>            
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Member</th>
-                            <th>Site</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>Chedeline Viljean</td>
-                            <td><a href="https://www.google.com" target="_blank" rel="noreferrer">google</a></td>
-                        </tr>
-                        <tr>
-                            <td>Nathan Yi</td>
-                            <td><a href="https://www.google.com" target="_blank" rel="noreferrer">google</a></td>
-                        </tr>
-                        <tr>
-                            <td>Omari Archer</td>
-                            <td><a href="https://www.google.com" target="_blank" rel="noreferrer">google</a></td>
-                        </tr>
-                        <tr>
-                            <td>Calvin Harris</td>
-                            <td><a href="https://www.linkedin.com/in/calvin-harris-jr" target="_blank" rel="noreferrer">LinkedIn</a></td>
-                        </tr>
-                    </tbody>
-                </table>            
+            <h2>Vineyards Dev Team:</h2>        
+            <p>Chedeline Viljean <a href="https://www.google.com" target="_blank" rel="noreferrer">google</a> | </p>
+            <p>Nathan Yi <a href="https://www.google.com" target="_blank" rel="noreferrer">google</a> | </p>      
+            <p>Omari Archer <a href="https://www.google.com" target="_blank" rel="noreferrer">google</a> | </p>
+            <p>Calvin Harris <a href="https://www.linkedin.com/in/calvin-harris-jr" target="_blank" rel="noreferrer">LinkedIn</a> | </p>
+                             
         </div>
     )
 }
